ef20acbac74e1a2fda33a85f73c2afb8cbd60390
IvanDM201 <ivanmicelli2004@gmail.com>
2025-11-04 11:18:28 -0400

    Cambios en el settings de base de datos

--- cocoatrace/cocoatrace/settings.py
+++ cocoatrace/cocoatrace/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'HOST': '10.10.10.25',
         'PORT': '1433',
         'OPTIONS': {
-            'driver': 'ODBC Driver 18 for SQL Server',
+            'driver': 'ODBC Driver 17 for SQL Server',
         },
     }
 }
